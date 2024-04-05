2f38f14e53d579c8f4b70c4e09be60906a6a7de1
Vyslon <santoni.thomas26@hotmail.fr>
2024-04-05 17:17:10 +0200

    gestion des tours + choix des armes

--- src/main.py
+++ src/main.py
@@ -3,91 +3,123 @@ from physical_objects import *
 from game_parameters import *
 
 
-def draw(screen, objects):
-	screen.fill(GameParameters.BACKGROUNDCOLOR)
-	
-	for o in objects:
-		o.moveFree()
-		pg.draw.circle(screen, GameParameters.WORMCOLOR, (o.x, o.y), o.radius)
-	
-	pg.display.flip()
+class Item(Enum):
+    PneumaticDrill = 0
+    Grenade = 1
+
+class State(Enum):
+    Moving = 0
+    InventoryOpen = 1
+
+def draw(screen, objects, inventoryOpen, sprites, selectedItem):
+    screen.fill(GameParameters.BACKGROUNDCOLOR)
+
+    for o in objects:
+        o.moveFree()
+        pg.draw.circle(screen, GameParameters.WORMCOLOR, (o.x, o.y), o.radius)
+
+    if (inventoryOpen):
+        portion_rect = pg.Rect(0 + 400 * selectedItem, 0 + 400 * selectedItem, 120, 120) # (400, 400),
+        image_portion = sprites.subsurface(portion_rect)
+        maxX, maxY = pg.display.get_surface().get_size()
+        screen.blit(image_portion, (maxX - 120, maxY - 120))
 
 
+    pg.display.flip()
 
 
 def mainloop(screen):
-	clock = pg.time.Clock()
+    clock = pg.time.Clock()
+    sprites = pg.image.load("data.png")
 
-	objects = []
-	# TODO : crash si NUMBEROFPLAYERS < 2 ?
-	for i in range(GameParameters.NUMBEROFPLAYERS):
-		w = Worm((i + 1) * 50, (i + 1) * 50)
-		w.deplacementVec.vx = 20
-		objects.append(w)
+    objects = []
+    items = [Item.PneumaticDrill, Item.Grenade, Item.PneumaticDrill, Item.Grenade]
+    selectedItem = 0
+    maxX, maxY = pg.display.get_surface().get_size()
+    # TODO : crash si NUMBEROFPLAYERS < 2 ?
+    for i in range(GameParameters.NUMBEROFPLAYERS):
+        w = Worm((i + 1) * 50, maxY - Worm.radius - 1)
+        #w.deplacementVec.vx = 20
+        objects.append(w)
 
-	hasFired = False
-	inventoryOpen = False
-	currentId = 0
-	turnClock = 0
+    hasFired = False
+    hasChanged = False
+    currentState = State.Moving
+    currentId = 0
+    turnClock = 0
 
-	running = True
+    running = True
 
-	while running:
-		xmax, ymax = pg.display.get_surface().get_size()
-		GameParameters.XMAX = xmax
-		GameParameters.YMAX = ymax
-		events = pg.event.get()
-		pressed = pg.key.get_pressed()
+    while running:
+        xmax, ymax = pg.display.get_surface().get_size()
+        GameParameters.XMAX = xmax
+        GameParameters.YMAX = ymax
+        events = pg.event.get()
+        pressed = pg.key.get_pressed()
 
-		# stops the program when closing
-		for event in events:
-			if event.type == pg.QUIT:
-				running = False
+        # stops the program when closing
+        for event in events: # TODO : KEYUP du space : on tire, pendant le keypressed, on charge, modifier ce qu'il y a à modifier
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.KEYDOWN:
+                if currentState == State.Moving:
+                    if event.key == pg.K_SPACE:
+                        objects[currentId].jump()
+                    elif event.key == pg.K_RSHIFT and not hasFired:
+                        currentState = State.InventoryOpen
+                else:
+                    if event.key == pg.K_RSHIFT:
+                        selectedItem = (selectedItem + 1) % len(items)
+                    elif event.key == pg.K_SPACE:
+                        if isinstance(items[selectedItem], Weapon):
+                            # TODO : faire un while pressed
+                            items[selectedItem].shot(100) # TODO : remplacer 100 par le pourcentage de "charge", passer les coordonnées du worms en param
+                        else:
+                            items[selectedItem].use() # TODO : passer les coordonnées du worms en param
+
+        if currentState == State.Moving:
+            if pressed[pg.K_q]:
+                objects[currentId].moveLeft()
+            elif pressed[pg.K_d]:
+                objects[currentId].moveRight()
+        else:
+            # todo : afficher arme
+            if pressed[pg.K_q]:
+                pass # TODO : viser vers la gauche
+            elif pressed[pg.K_d]:
+                pass  # TODO : viser vers la droite
 
 
-		if not(inventoryOpen):
-			if pressed[pg.K_q]:
-				objects[currentId].moveLeft()
-			elif pressed[pg.K_d]:
-				objects[currentId].moveRight()
-			elif pressed[pg.K_SPACE]:
-				objects[currentId].jump()
-			elif pressed[pg.K_RSHIFT]:
-				inventoryOpen = True
-		else:
-			if pressed[pg.K_RSHIFT]:
-				pass
-				# TODO : change d'arme
+        # maj(objects)
+        for i in range(GameParameters.NUMBEROFPLAYERS):
+            objects[i].refreshState()
 
-		# TODO : if arm selected : on peut tirer avec, Q et D change la visée de l'arme
+        draw(screen, objects, currentState == State.InventoryOpen, sprites, selectedItem)
 
-		# maj(objects)
-		for i in range(GameParameters.NUMBEROFPLAYERS):
-			objects[i].refreshState()
+        clock.tick(40)
+        turnClock += 40
+        if (turnClock >= GameParameters.NUMBERMILLISECONDSTURN) or hasFired:
+            currentId = (currentId + 1) % GameParameters.NUMBEROFPLAYERS
+            turnClock = 0
+            currentState = State.Moving
+            print("Nouveau tour")
 
-		draw(screen, objects)
-		
-		clock.tick(40)
-		turnClock += 40
-		if (turnClock >= GameParameters.NUMBERMILLISECONDSTURN) or hasFired:
-			currentId = (currentId + 1) % GameParameters.NUMBEROFPLAYERS
-			turnClock = 0
-	
-	print("Fermeture du programme")
-	pg.quit()
-	quit()
+    print("Fermeture du programme")
+    pg.quit()
+    quit()
 
 
 def screenInit():
-	print("\nProgram initialisation...")
-	
-	pg.init()
-	screen = pg.display.set_mode((900, 600), pg.RESIZABLE)
-	screen.fill(GameParameters.BACKGROUNDCOLOR)
-	pg.display.flip()
-	
-	return screen
+    print("\nProgram initialisation...")
+
+    pg.init()
+    screen = pg.display.set_mode((900, 600), pg.RESIZABLE)
+    screen.fill(GameParameters.BACKGROUNDCOLOR)
+    pg.display.flip()
+
+    return screen
+
 
 if __name__ == "__main__":
-	screen = screenInit()
-	mainloop(screen)
+    screen = screenInit()
+    mainloop(screen)
--- src/physical_objects.py
+++ src/physical_objects.py
@@ -1,82 +1,114 @@
 from game_parameters import *
 from enum import Enum
 
+
 class WormState(Enum):
-	GROUNDED = 0
-	AIRBORNE = 1
+    GROUNDED = 0
+    AIRBORNE = 1
+
 
 class Vector:
-	
-	def __init__(self, vx, vy):
-		self.vx = vx
-		self.vy = vy
-	
-	def add(self, v):
-		self.vx += v.vx
-		self.vy += v.vy
-	
-	def __str__(self):
-		return "(" + str(self.vx) + ", " + str(self.vy) + ")"
+
+    def __init__(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
+
+    def add(self, v):
+        self.vx += v.vx
+        self.vy += v.vy
+
+    def __str__(self):
+        return "(" + str(self.vx) + ", " + str(self.vy) + ")"
 
 
 class PhysicalSphere:
-	
-	gravityVector = Vector(0, GameParameters.GRAVITY)
-	
-	def __init__(self, x, y, radius):
-		self.x = x
-		self.y = y
-		self.radius = radius
-		self.deplacementVec = Vector(0, 0)
-	
-	def moveFree(self):
-		self.deplacementVec.add(self.gravityVector)
-		self.handleCollision()
-		print(self.deplacementVec)
-		self.x += self.deplacementVec.vx
-		self.y += self.deplacementVec.vy
-	
-	def handleCollision(self):
-		stuckGround = False
-		if (self.x + self.radius + self.deplacementVec.vx > GameParameters.XMAX) or (self.x - self.radius + self.deplacementVec.vx < GameParameters.XMIN):
-			self.deplacementVec.vx = -self.deplacementVec.vx * GameParameters.BOUNCINGABSORPTION
-			self.deplacementVec.vy *= GameParameters.BOUNCINGABSORPTION
-			
-		if (self.y + self.radius + self.deplacementVec.vy > GameParameters.YMAX) or (self.y - self.radius + self.deplacementVec.vy < GameParameters.YMIN):
-			if (self.y + self.radius + self.deplacementVec.vy > GameParameters.YMAX) and (self.deplacementVec.vy < 2):
-				stuckGround = True
-			self.deplacementVec.vx *= GameParameters.BOUNCINGABSORPTION
-			self.deplacementVec.vy = -self.deplacementVec.vy * GameParameters.BOUNCINGABSORPTION
-		
-		# colle l'objet au sol
-		if stuckGround :
-			self.deplacementVec.vx = 0
-			self.deplacementVec.vy = 0
+    gravityVector = Vector(0, GameParameters.GRAVITY)
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.deplacementVec = Vector(0, 0)
+
+    def moveFree(self):
+        self.deplacementVec.add(self.gravityVector)
+        self.handleCollision()
+        print(self.deplacementVec)
+        self.x += self.deplacementVec.vx
+        self.y += self.deplacementVec.vy
+
+    def handleCollision(self):
+        stuckGround = False
+        if (self.x + self.radius + self.deplacementVec.vx > GameParameters.XMAX) or (
+                self.x - self.radius + self.deplacementVec.vx < GameParameters.XMIN):
+            self.deplacementVec.vx = -self.deplacementVec.vx * GameParameters.BOUNCINGABSORPTION
+            self.deplacementVec.vy *= GameParameters.BOUNCINGABSORPTION
+
+        if (self.y + self.radius + self.deplacementVec.vy > GameParameters.YMAX) or (
+                self.y - self.radius + self.deplacementVec.vy < GameParameters.YMIN):
+            if (self.y + self.radius + self.deplacementVec.vy > GameParameters.YMAX) and (self.deplacementVec.vy < 2):
+                stuckGround = True
+            self.deplacementVec.vx *= GameParameters.BOUNCINGABSORPTION
+            self.deplacementVec.vy = -self.deplacementVec.vy * GameParameters.BOUNCINGABSORPTION
+
+        # colle l'objet au sol
+        if stuckGround:
+            self.deplacementVec.vx = 0
+            self.deplacementVec.vy = 0
 
 
 class Worm(PhysicalSphere):
-	slideSpeed = 3
-	radius = 10
-	
-	def __init__(self, x, y):
-		PhysicalSphere.__init__(self, x, y, 10)
-		self.state = WormState.GROUNDED
-	
-	def moveRight(self):
-		if self.state == WormState.GROUNDED:
-			self.x += self.slideSpeed
-	
-	def moveLeft(self):
-		if self.state == WormState.GROUNDED:
-			self.x -= self.slideSpeed
+    slideSpeed = 4
+    radius = 10
 
-	def jump(self):
-		if self.state == WormState.GROUNDED:
-			self.deplacementVec.vy = GameParameters.JUMPPOWER
-			self.state = WormState.AIRBORNE
+    def __init__(self, x, y):
+        PhysicalSphere.__init__(self, x, y, 10)
+        self.state = WormState.GROUNDED
 
-	def refreshState(self):
-		if True: #TODO : si on touche le sol
-			self.state = WormState.GROUNDED
-		else:
-			self.state = WormState.AIRBORNE
+    def moveRight(self):
+        if self.state == WormState.GROUNDED:
+            self.x += self.slideSpeed
+
+    def moveLeft(self):
+        if self.state == WormState.GROUNDED:
+            self.x -= self.slideSpeed
+
+    def jump(self):
+        if self.state == WormState.GROUNDED:
+            self.deplacementVec.vy = GameParameters.JUMPPOWER
+            self.state = WormState.AIRBORNE
+
+    def refreshState(self):
+        if True:  # TODO : si on touche le sol
+            self.state = WormState.GROUNDED
+        else:
+            self.state = WormState.AIRBORNE
+
+
+# class WeaponType(Enum):
+#	LAUNCHED = 0
+#	Melee = 1
+#	Thrown = 2
+
+class Utility:
+    def __init__(self):
+        pass
+
+class PneumaticDrill(Utility):
+    def __init__(self):
+        Utility.__init__(self)
+
+    def use(self):
+        pass
+
+class Weapon:
+    def __init__(self, damage):
+        self.damage = damage
+
+    def shot(self, power):
+        pass  # TODO
+
+
+class Grenade(Weapon):
+    def __init__(self):
+        Weapon.__init__(self, 50)
