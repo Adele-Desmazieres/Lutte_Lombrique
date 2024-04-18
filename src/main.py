5d8a9dcd26f7e09db0b1a12075f593efc47e0a27
Delta-Chat <adele.desmazi@gmail.com>
2024-04-18 20:50:21 +0200

    Refactoring main

--- src/main.py
+++ src/main.py
@@ -10,198 +10,179 @@ from map2D import *
 from weapon import *
 from inventory import *
 from explosion import *
+from model import *
 
 
-BACKGROUNDIMAGE = None
-PYGAMEBACKGROUNDIMAGE = None
+TERRAIN_IMG = None
+PG_TERRAIN_IMG = None
 
 
-class GameState(Enum):
-    INTERACTIVE = 1
-    ANIMATION = 2
-
-class State(Enum):
-    Moving = 0
-    InventoryOpen = 1
-
-
-
-def draw(screen, terrain, worms, objects, inventoryOpen, inventory, rangedWeapons, currentId):
-    
+def draw(screen, game):
     screen.fill(Settings.BACKGROUNDCOLOR)
-    screen.blit(PYGAMEBACKGROUNDIMAGE, (0,0))
+    screen.blit(PG_TERRAIN_IMG, (0,0))
             
-    for s in terrain.surfaces:
+    for s in game.terrain.surfaces:
         pg.draw.line(screen, (200, 200, 200), s.p, s.q, width=3)
-        # m, n = s.normalVectorSegmentMiddle()
-        # pg.draw.line(screen, (250, 50, 50), m, n, width=1)
 
-    for w in worms:
+    for w in game.worms:
         w.moveFree()
         w.draw(screen)
 
-    for o in objects:
+    for o in game.objects:
         o.moveFree()
         o.draw(screen)
 
-    if (inventoryOpen):
-        inventory.draw(screen)
+    if (game.inventoryOpenState == InventoryState.Opened):
+        game.inventory.draw(screen)
 
-    if inventory.currentItem() in rangedWeapons:
-        worms[currentId].draw_aiming_cursor(screen)
+    if game.inventory.currentItem() in game.rangedWeapons:
+        game.worms[game.current_worm_id].draw_aiming_cursor(screen)
 
     pg.display.flip()
 
 
-def mainloop(screen):
-    global BACKGROUNDIMAGE
-    global PYGAMEBACKGROUNDIMAGE
+def initGameValues(game):
+    game.initMap() # also initialize generation_threshold and square_size
+    game.initTerrain()
+    
+    game.worms = []
+    game.current_worm_id = 0
+    game.worm_has_fired = False
+    game.objects = []
+    game.rangedWeapons = [Item.Grenade]
+    
+    game.inventory = Inventory()
+    game.inventoryOpenState = InventoryState.Closed
+    
+    game.clock = pg.time.Clock()
+    game.state = GameState.INTERACTIVE
+    game.turnTimer = 0
+
+
+
+def mainloop(screen, game):
+    global TERRAIN_IMG
+    global PG_TERRAIN_IMG
     
     xmax, ymax = pg.display.get_surface().get_size()
     Settings.XMAX = xmax
     Settings.YMAX = ymax
-    clock = pg.time.Clock()
     
-    # map = [[0, 0, 0, 0, 0, 0, 0], 
-    #        [0, 1, 1, 1, 0, 0, 0],
-    #        [0, 1, 1, 1, 1, 0, 0],
-    #        [0, 1, 1, 1, 1, 1, 0],
-    #        [0, 0, 0, 0, 0, 0, 0]
-    #        ]
-    # generation_threshold = 0.5
-    # square_size = 60
+    initGameValues(game)
     
-    map = Map2D(90, 60, 0, 10).getCoefsFormatted()
-    generation_threshold = 5
-    square_size = min(Settings.YMAX/(len(map[0])-1), Settings.XMAX/(len(map)-1))
+    TERRAIN_IMG = Image.open('../img/terrain4.jpg')
+    TERRAIN_IMG = TERRAIN_IMG.resize((xmax, ymax))
+    # pg.transform.scale(TERRAIN_IMG, (xmax, ymax))
     
-    terrain = Terrain(map, square_size, generation_threshold)
-    print(len(terrain.surfaces))
-    print(len(terrain.polygons))
-	
-    BACKGROUNDIMAGE = Image.open('../img/terrain4.jpg')
-    BACKGROUNDIMAGE = BACKGROUNDIMAGE.resize((xmax, ymax))
-    # pg.transform.scale(BACKGROUNDIMAGE, (xmax, ymax))
-    
-    PYGAMEBACKGROUNDIMAGE = pg.Surface((xmax, ymax), pg.SRCALPHA)
-    for polygon in terrain.polygons:
+    PG_TERRAIN_IMG = pg.Surface((xmax, ymax), pg.SRCALPHA)
+    for polygon in game.terrain.polygons:
         if len(polygon) > 2:
             # pg.draw.polygon(screen, (rd.randrange(255), rd.randrange(255), rd.randrange(255)), polygon)
             # pg.draw.polygon(screen, (200, 150, 50), polygon)
-                
+            
             # creating the mask
-            mask = Image.new('RGBA', BACKGROUNDIMAGE.size)
+            mask = Image.new('RGBA', TERRAIN_IMG.size)
             d = ImageDraw.Draw(mask)
             d.polygon(polygon, fill='#000')
     
-            out = Image.new('RGBA', BACKGROUNDIMAGE.size)
-            out.paste(BACKGROUNDIMAGE, (0,0), mask)
+            out = Image.new('RGBA', TERRAIN_IMG.size)
+            out.paste(TERRAIN_IMG, (0,0), mask)
             image_data = out.tobytes()
             image_dimensions = out.size
             pygame_surface = pg.image.fromstring(image_data, image_dimensions, 'RGBA')
             pygame_surface = pygame_surface.convert_alpha()
-            PYGAMEBACKGROUNDIMAGE.blit(pygame_surface, (0,0))
+            PG_TERRAIN_IMG.blit(pygame_surface, (0,0))
     
-    
-    state = GameState.INTERACTIVE
-    worms = []
-    objects = []
-    rangedWeapons = [Item.Grenade]
-    inventory = Inventory()
     
     if Settings.NUMBEROFPLAYERS < 2:
         exit()
     for i in range(Settings.NUMBEROFPLAYERS):
         w = Worm((i + 1) * 50, ymax - Worm.radius - 1)
-        #w.deplacementVec.vx = 20
-        worms.append(w)
-
-    hasFired = False
-    currentState = State.Moving
-    currentId = 0
-    turnClock = 0
+        game.worms.append(w)
 
     running = True
-
     while running:
         events = pg.event.get()
         pressed = pg.key.get_pressed()
 
-        if state == GameState.INTERACTIVE:
+        if game.state == GameState.INTERACTIVE:
             for event in events:
                 # stops the program when closing
                 if event.type == pg.QUIT:
                     running = False
                     
                 if event.type == pg.KEYDOWN:
-                    if currentState == State.Moving:
+                    # stops the program on clicking the escape button
+                    if event.key == pg.K_ESCAPE:
+                        running = False
+                    
+                    if game.inventoryOpenState == InventoryState.Closed:
                         if event.key == pg.K_SPACE:
-                            worms[currentId].jump()
-                        elif event.key == pg.K_RSHIFT and not hasFired:
-                            currentState = State.InventoryOpen
+                            game.worms[game.current_worm_id].jump()
+                        elif event.key == pg.K_RSHIFT and not game.worm_has_fired:
+                            game.inventoryOpenState = InventoryState.Opened
                     else:
                         if event.key == pg.K_RSHIFT:
-                            inventory.changeSelectedItem()
-                            
-                if event.type == pg.KEYUP and currentState == State.InventoryOpen:
+                            game.inventory.changeSelectedItem()
+                       
+                if event.type == pg.KEYUP and game.inventoryOpenState == InventoryState.Opened:
                     if event.key == pg.K_SPACE:
-                        inventory.triggerCurrentItem(worms[currentId], objects)
-                        state = GameState.ANIMATION # TODO : seulement pour les weapons ou les rangedWeapons?
-                        hasFired = True
-
-            if currentState == State.Moving:
+                        game.inventory.triggerCurrentItem(game.worms[game.current_worm_id], game.objects)
+                        game.state = GameState.ANIMATION # TODO : seulement pour les weapons ou les rangedWeapons?
+                        game.worm_has_fired = True
+            
+            if game.inventoryOpenState == InventoryState.Closed:
                 if pressed[pg.K_q]:
-                    worms[currentId].moveLeft()
+                    game.worms[game.current_worm_id].moveLeft()
                 elif pressed[pg.K_d]:
-                    worms[currentId].moveRight()
+                    game.worms[game.current_worm_id].moveRight()
                     
-            elif inventory.currentItem() in rangedWeapons:
+            elif game.inventory.currentItem() in game.rangedWeapons:
                 if pressed[pg.K_q]:
-                    worms[currentId].aimLeft()
-                    print(worms[currentId].aimAngle)
+                    game.worms[game.current_worm_id].aimLeft()
+                    # print(game.worms[game.current_worm_id].aimAngle)
 
                 if pressed[pg.K_d]:
-                    worms[currentId].aimRight()
-                    print(worms[currentId].aimAngle)
+                    game.worms[game.current_worm_id].aimRight()
+                    # print(game.worms[game.current_worm_id].aimAngle)
 
                 if pressed[pg.K_SPACE]:
-                    worms[currentId].charge()
+                    game.worms[game.current_worm_id].charge()
 
 
-        for i in range(len(worms)):
-            worms[i].refreshState()
+        for i in range(len(game.worms)):
+            game.worms[i].refreshState()
 
-        for obj in objects:
+        for obj in game.objects:
             if isinstance(obj, Grenade):
                 if pg.time.get_ticks() - obj.creation_tick > 5000:
                     # TODO: boom animation + appliquer dégâts au terrain
                     Explosion.draw_explosion(screen, (obj.x, obj.y))
-                    obj.explode(worms)
-                    state = GameState.INTERACTIVE
-                    objects.remove(obj)
+                    obj.explode(game.worms)
+                    game.objects.remove(obj)
+                    game.state = GameState.INTERACTIVE
 
-        for w in worms:
+        for w in game.worms:
             if w.hp <= 0:
-                worms.remove(w)
+                game.worms.remove(w)
 
-        if len(worms) <= 1:
-            print("Fin de la partie")
-            break
-        
         # TODO: si plus qu'un seul wormms, lui attribuer la victoire
+        if len(game.worms) <= 1:
+            print("Fin de la partie")
+            running = False
 
-        draw(screen, terrain, worms, objects, currentState == State.InventoryOpen, inventory, rangedWeapons, currentId)
+        draw(screen, game)
 
-        clock.tick(40)
-        turnClock += 40
-        if (turnClock >= Settings.NUMBERMILLISECONDSTURN) or hasFired:
-            worms[currentId].powerCharge = 0
-            worms[currentId].aimAngle = -90
-            currentId = (currentId + 1) % len(worms)
-            print("currentId : {}".format(currentId))
-            turnClock = 0
-            currentState = State.Moving
-            hasFired = False
+        game.clock.tick(40)
+        game.turnTimer += 40
+        if (game.turnTimer >= Settings.NUMBERMILLISECONDSTURN) or game.worm_has_fired:
+            game.worms[game.current_worm_id].powerCharge = 0
+            game.worms[game.current_worm_id].aimAngle = -90
+            game.current_worm_id = (game.current_worm_id + 1) % len(game.worms)
+            print("current_worm_id : {}".format(game.current_worm_id))
+            game.turnTimer = 0
+            game.inventoryOpenState = InventoryState.Closed
+            game.worm_has_fired = False
             print("Nouveau tour")
 
     print("Fermeture du programme")
@@ -225,6 +206,7 @@ if __name__ == "__main__":
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
-
+    
     screen = screenInit()
-    mainloop(screen)
+    game = Model()
+    mainloop(screen, game)
---
+++ src/model.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from map2D import *
+from terrain import *
+from settings import *
+
+class GameState(Enum):
+    INTERACTIVE = 1
+    ANIMATION = 2
+
+class InventoryState(Enum):
+    Closed = 0
+    Opened = 1
+
+class Model:
+    
+    def __init__(self):
+        self.map = None
+        self.generation_threshold = None
+        self.square_size = None
+        self.terrain = None
+        
+        self.worms = None
+        self.current_worm_id = None
+        self.worm_has_fired = None
+        self.objects = None
+        self.rangedWeapons = None
+        
+        self.inventory = None
+        self.inventoryOpenState = None
+        
+        self.clock = None
+        self.state = None
+        self.turnTimer = None
+        
+        # self.currentState = None
+    
+    def initMap(self):
+        # map = [[0, 0, 0, 0, 0, 0, 0], 
+        #        [0, 1, 1, 1, 0, 0, 0],
+        #        [0, 1, 1, 1, 1, 0, 0],
+        #        [0, 1, 1, 1, 1, 1, 0],
+        #        [0, 0, 0, 0, 0, 0, 0]
+        #        ]
+        # generation_threshold = 0.5
+        # square_size = 60
+    
+        self.map = Map2D(90, 60, 0, 10).getCoefsFormatted()
+        self.generation_threshold = 5
+        self.square_size = min(Settings.YMAX/(len(self.map[0])-1), Settings.XMAX/(len(self.map)-1))
+    
+    def initTerrain(self):
+        self.terrain = Terrain(self.map, self.square_size, self.generation_threshold)
+    
